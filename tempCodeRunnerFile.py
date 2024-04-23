383ce7bdf9204d834c0cfe5988504fe400bcba39
Jackson Stephan <121452776+Jackz531@users.noreply.github.com>
2024-04-23 16:38:12 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-if (datetime.now() - start_time).total_seconds() > 40:
-            global_df.to_csv("int.csv")
-            is_program_running=False
-            subprocess.run(['python', 'libcalls.py'])
