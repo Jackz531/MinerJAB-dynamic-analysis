6cf2a0485c327aba775c80466e2a8c830557b15b
Jackson Stephan <121452776+Jackz531@users.noreply.github.com>
2024-04-23 16:37:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,15 +9,15 @@ clf_loaded = joblib.load('random_forest.joblib')
 # Load your test data
 test_data = pd.read_csv('test_data.csv')
 
-# Encode the 'Cryptojacker or not' column to numerical values
+# Encode the 'Crytpojacker or not' column to numerical values
 # This assumes that 'yes' is encoded as 1 and 'no' as 0
 label_encoder = LabelEncoder()
 test_data['Cryptojacker or not'] = label_encoder.fit_transform(test_data['Cryptojacker or not'])
 
 # Prepare the data for testing
-X_test = test_data[['median_cpu_percent','quadratic_deviation','ram_usage','upload_speed','median_network_rate','download_speed','Number of crypto API calls']]
+X_test = test_data[['median_cpu_percent', 'quadratic_deviation', 'ram_usage', 'median_network_rate', 'Number of crypto API calls']]
 PIDs = test_data['pid']
-
+y_test = test_data['Cryptojacker or not']
 # Make predictions on the test data
 predictions = clf_loaded.predict(X_test)
 
@@ -30,9 +30,11 @@ recall = recall_score(test_data['Cryptojacker or not'], predictions)
 f1 = f1_score(test_data['Cryptojacker or not'], predictions)
 conf_matrix = confusion_matrix(test_data['Cryptojacker or not'], predictions)
 
-for pid, prediction in zip(PIDs, test_data['Predicted']):
-    print(f'PID: {pid}, Predicted Label: {prediction}')
-    
+# for pid,prediction in zip(PIDs, test_data['Predicted']):
+#     print(f'PID: {pid}, Predicted Label: {prediction}')
+for pid, actual, predicted in zip(PIDs, y_test, test_data['Predicted']):
+    actual_label = label_encoder.inverse_transform([actual])[0]
+    print(f'PID: {pid}, Actual Label: {actual_label}, Predicted Label: {predicted}')   
 # Print the performance metrics
 print(f'Accuracy: {accuracy}')
 print(f'Recall: {recall}')
@@ -41,6 +43,6 @@ print(f'Confusion Matrix:\n{conf_matrix}')
 
 # Save the test data with predictions to a new CSV file
 test_data.to_csv('test_data_with_predictions.csv', index=False)
-import subprocess
+# import subprocess
 
-subprocess.run(['python', 'terminate.py'])
+# subprocess.run(['python', 'terminate.py'])
