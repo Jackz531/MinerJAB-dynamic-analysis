ee46be10f20c001195d96b669731427393a27111
Jackson Stephan <121452776+Jackz531@users.noreply.github.com>
2024-05-04 17:54:30 +0530

    Update train.py

--- random forest/train.py
+++ random forest/train.py
@@ -2,20 +2,23 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 import joblib
+from sklearn import tree
+import matplotlib.pyplot as plt
 
 # Load your training data from a CSV file
-train_data = pd.read_csv('train_data.csv')
+train_data = pd.read_csv('train_data3.csv')
 
-# Encode the 'Crytpojacker or not' column to numerical values
+# Encode the 'Cryptojacker or not' column to numerical values
 label_encoder = LabelEncoder()
-train_data['Crytpojacker or not'] = label_encoder.fit_transform(train_data['Crytpojacker or not'])
+train_data['Cryptojacker or not'] = label_encoder.fit_transform(train_data['Cryptojacker or not'])
 
 # Prepare the data for training
-X_train = train_data.drop(['PID', 'Crytpojacker or not'], axis=1)
-y_train = train_data['Crytpojacker or not']
+# Ensure the column names match the ones in your CSV file
+X_train = train_data.drop(['pid', 'Cryptojacker or not'], axis=1)
+y_train = train_data['Cryptojacker or not']
 
 # Initialize the Random Forest Classifier
-clf = RandomForestClassifier(n_estimators=100, random_state=42)
+clf = RandomForestClassifier(n_estimators=50, max_depth=5, min_samples_split=5, min_samples_leaf=2, random_state=42)
 
 # Train the classifier
 clf.fit(X_train, y_train)
